ee60cf735ec89c6fccee9a27328aed28b572d927
Your Name <nicode611@gmail.com>
2023-10-01 18:19:13 +0200

    18:19

---
+++ pages/settings.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <title>Parametres</title>
+</head>
+<body>
+
+<?php
+        $includeFile = "../includes/navigation.php";
+        if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
+    ?>
+    
+</body>
+</html>
