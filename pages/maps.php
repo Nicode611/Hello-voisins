a4a424560b1d7106968b22d4095c34f7e9c54fbe
Your Name <nicode611@gmail.com>
2023-10-02 13:49:54 +0200

    13:49

--- pages/maps.php
+++ pages/maps.php
@@ -4,14 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="../assets/css/maps.css">
     <title>Carte</title>
 </head>
 <body>
 
-<?php
+    <?php
         $includeFile = "../includes/navigation.php";
         if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
     ?>
+
+    <div class="map-container">
+        <div class="map">
+
+        </div>
+    </div>
     
 </body>
 </html>
