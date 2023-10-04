e70c1786b6ace32e11c9adca33290ef580e4ec3c
Your Name <nicode611@gmail.com>
2023-10-04 14:26:58 +0200

    14:26

---
+++ pages/self-profile.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="../assets/css/self-profile.css">
+    
+    <title>Chat de proximité</title>
+</head>
+<body>
+    
+    <?php
+        $includeFile = "../includes/navigation.php";
+        if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
+    ?>
+
+    <div class="main-content">
+        
+    </div>
+
+</body>
+</html>
