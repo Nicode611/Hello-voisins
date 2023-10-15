4881a7e81f8afbbe18215ceef237b9784759425f
Your Name <nicode611@gmail.com>
2023-10-15 18:03:21 +0200

    18:03

--- pages/proximity-chat.php
+++ pages/proximity-chat.php
@@ -42,7 +42,10 @@
 
 
     <script> 
-        var conn = new WebSocket('wss://hello-voisins-25649417130d.herokuapp.com');
+    <?php
+        $port = getenv('PORT') ? getenv('PORT') : 8080;
+    ?>
+        var conn = new WebSocket('wss://hello-voisins-25649417130d.herokuapp.com:<?php echo $port; ?>');
 conn.onopen = function(e) {
     console.log("Connection established!");
 };
