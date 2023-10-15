0078af914c961e8739c276ac772d4315fc45463b
Your Name <nicode611@gmail.com>
2023-10-15 13:59:58 +0200

    13:59

--- config/Chat.php
+++ config/Chat.php
@@ -1,51 +1,51 @@
 <?php
 namespace MyApp;
+
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use Monolog\Logger; // Importez la classe Logger de Monolog
 
 class Chat implements MessageComponentInterface {
     protected $clients;
+    protected $logger; // Ajoutez une propriété pour le logger Monolog
     
-
-    public function __construct() {
+    public function __construct(Logger $logger) {
         $this->clients = new \SplObjectStorage;
+        $this->logger = $logger; // Initialisez le logger
     }
-
-    public function isLastClient() {
-        return count($this->clients) == 1;
-    }
-    
 
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";
+    
+        $this->logger->info("New connection! ({$conn->resourceId})");
     }
-
+    
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
+    
         foreach ($this->clients as $client) {
             if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
                 $client->send($msg);
             }
         }
+    
+        $this->logger->info(sprintf('Connection %d sending message "%s" to %d other connection%s', $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's'));
     }
+    
 
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
 
-        echo "Connection {$conn->resourceId} has disconnected\n";
-
+        // Utilisez le logger pour enregistrer la fermeture de connexion
+        $this->logger->info("Connection {$conn->resourceId} has disconnected");
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
+        // Utilisez le logger pour enregistrer les erreurs
+        $this->logger->error("An error has occurred: " . $e->getMessage());
+
 
         $conn->close();
     }
--- config/server.php
+++ config/server.php
@@ -1,31 +1,25 @@
 <?php
-
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-
-$logger = new Logger('ratchet');
-$logger->pushHandler(new StreamHandler('php://stdout', Logger::INFO));
-
-
-$port = $_SERVER['PORT']; 
-
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use \MyApp\Chat;
 
+$logger = new Logger('ratchet');
+$logger->pushHandler(new StreamHandler('php://stdout', Logger::INFO));
+
+$port = $_SERVER['PORT']; 
+
+$chat = new Chat($logger); // Passer le logger à la classe Chat
 
 $server = IoServer::factory(
     new HttpServer(
-        new WsServer(
-            new Chat() // Instanciez la classe Chat avec la référence à l'objet serveur
-        )
+        new WsServer($chat) // Utiliser l'instance de Chat avec le logger
     ),
     $port
 );
 
 $server->run();
-
-
