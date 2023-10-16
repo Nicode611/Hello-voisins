d76b7cd715f31ab97b2edf44e8431afb35cfa08c
Your Name <nicode611@gmail.com>
2023-10-16 11:41:41 +0200

    11:41

--- config/server.php
+++ config/server.php
@@ -3,21 +3,19 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Ratchet\Server\IoServer;
-// use Ratchet\Http\HttpServer;
-// use Ratchet\WebSocket\WsServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 use \MyApp\Chat;
 
 $port = getenv('PORT') ? getenv('PORT') : 8080;
 
 
-// $server = IoServer::factory(
-//     new HttpServer(
-//         new WsServer()
-//     ),
-//     $port
-// );
 $server = IoServer::factory(
-    new Chat(),
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
     $port
 );
 
