2e5ea425a34f1fd04effb9ced105d4ca16d94ed0
Your Name <nicode611@gmail.com>
2023-10-21 02:09:37 +0200

    02:09

--- config/server.php
+++ config/server.php
@@ -9,32 +9,16 @@ use \MyApp\Chat;
 
 $port = 8080;
 
-// Configuration du contexte SSL/TLS
-$context = stream_context_create([
-    'ssl' => [
-        'local_cert' => '/etc/letsencrypt/live/hello-voisins.com/fullchain.pem',
-        'local_pk' => '/etc/letsencrypt/live/hello-voisins.com/privkey.pem',
-        'verify_peer' => false,
-    ]
-]);
 
-// Lier le serveur WebSocket au contexte SSL/TLS
-$socket = stream_socket_server("ssl://0.0.0.0:{$port}", $errno, $errstr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, $context);
-
-if (!$socket) {
-    echo "Erreur lors de la création du socket SSL : $errstr ($errno)\n";
-} else {
-    echo "Serveur WebSocket sécurisé en cours d'exécution sur le port {$port}\n";
-}
-
-// Créer le serveur Ratchet WebSocket
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new Chat()
         )
     ),
-    $socket
+    8080
 );
+
+
 
 $server->run();
