f290f9c5b54dd20ff97822ab6c93194f0b45682d
Your Name <nicode611@gmail.com>
2023-10-30 11:10:03 +0100

    11:09

---
+++ config/AWS/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    aws_project_region: 'Europe (Paris) eu-west-3',
+    aws_s3_bucket: 'hello-voisins', // Nom du compartiment S3
+    aws_s3_region: 'Europe (Paris) eu-west-3',
+  };
+  
+  export default awsmobile;
+  
