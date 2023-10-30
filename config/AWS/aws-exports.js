196d343784589a0bc28738e958bb655bf3741bc6
Your Name <nicode611@gmail.com>
2023-10-30 12:54:30 +0100

    12:53

--- config/AWS/aws-exports.js
+++ config/AWS/aws-exports.js
@@ -1,8 +1,7 @@
-const awsmobile = {
-    aws_project_region: 'Europe (Paris) eu-west-3',
-    aws_s3_bucket: 'hello-voisins', // Nom du compartiment S3
-    aws_s3_region: 'Europe (Paris) eu-west-3',
-  };
-  
-  export default awsmobile;
-  
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: 'VOTRE_ACCESS_KEY_ID',
+  secretAccessKey: 'VOTRE_SECRET_ACCESS_KEY',
+  region: 'VOTRE_REGION_AWS',
+});
