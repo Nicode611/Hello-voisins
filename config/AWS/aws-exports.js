660eb4948e403dfc2c0591a472f7059424f7e94c
Your Name <nicode611@gmail.com>
2023-10-30 13:31:17 +0100

    13:31

--- config/AWS/aws-exports.js
+++ config/AWS/aws-exports.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  accessKeyId: 'VOTRE_ACCESS_KEY_ID',
-  secretAccessKey: 'VOTRE_SECRET_ACCESS_KEY',
-  region: 'VOTRE_REGION_AWS',
+  accessKeyId: 'AKIAZH3QK4AOPRWHOBHM',
+  secretAccessKey: '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',
+  region: 'Europe (Paris) eu-west-3',
 });
