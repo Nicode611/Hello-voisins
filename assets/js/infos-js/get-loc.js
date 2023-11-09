692866e3c432003daaaa95a4893e4cea3673445d
Your Name <nicode611@gmail.com>
2023-11-09 02:25:55 +0100

    02:25

--- assets/js/infos-js/get-loc.js
+++ assets/js/infos-js/get-loc.js
@@ -1,25 +1,4 @@
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        // Récupère la localisation
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
 
-        // Vérifiez si les valeurs de latitude et longitude sont définies et valides
-        // Les valeurs sont définies et valides, vous pouvez envoyer la requête AJAX
-            
-        $.ajax({
-            type: "POST",
-            url: "../scripts/infos-scripts/send-loc.php",
-            data: { 
-                latitude: latitude,
-                longitude: longitude
-            },
-            success: function(response) {
-                console.log('mis a jour')
-            }
-        });
-});
-}
 
 
 refreshButton = document.querySelector('.refresh-position');
