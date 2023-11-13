7c6169c4ff8ab1c5efaf546c985e9520b85d69ec
Your Name <nicode611@gmail.com>
2023-11-13 03:15:54 +0100

    03:15

--- pages/proximity-chat.php
+++ pages/proximity-chat.php
@@ -108,7 +108,10 @@
                                 // console.log("La position 2 est dans un rayon de 500 mètres de la position 1.");
                                 if (data.message === "S'est déconnecté." || data.message === "S'est connecté.") {
                                     console.log('s\'est déco ou reco');
-                                    latitude, longitude, data.messageLatitude, data.messageLongitude = null;
+                                    latitude = null; 
+                                    longitude = null;
+                                    data.messageLatitude = null;
+                                    data.messageLongitude = null;
                                     appendReceivedMessage(data.username, data.message, data.id, data.profileImgPath);
                                 } else {
                                     appendReceivedMessage(data.username, data.message, data.id, data.profileImgPath, latitude, longitude, data.messageLatitude, data.messageLongitude);
