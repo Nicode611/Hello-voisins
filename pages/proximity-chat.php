0204178c142b7b17fcd5e5ac330cffc58096a761
Your Name <nicode611@gmail.com>
2023-11-13 01:05:54 +0100

    01:05

--- pages/proximity-chat.php
+++ pages/proximity-chat.php
@@ -58,49 +58,8 @@
             myId = ' <?php echo $id = $_SESSION['user_id']; ?>';
             profileImgPath = '<?php echo $_SESSION['user_profile_img_path']; ?>';
             channelId = "";
-            
-            // Options de configuration (facultatives)
-            var options = {
-                enableHighAccuracy: true, // Utiliser la meilleure précision possible
-                timeout: 5000,            // Temps maximal pour obtenir la position en millisecondes
-                maximumAge: 0             // Ne pas utiliser de position mise en cache
-            };
-
             if ("geolocation" in navigator) {
-                // Lance la surveillance de la position
-                var watchId = navigator.geolocation.watchPosition(validPosition, invalidPosition, options);
-            } else {
-                console.error("La géolocalisation n'est pas disponible dans ce navigateur.");
-            }
-
-
-            function invalidPosition(error) {
-                var mainContent = document.querySelector('.main-content');
-                switch (error.code) {
-                    case error.PERMISSION_DENIED:
-                        // L'utilisateur a refusé la demande de géolocalisation
-                        mainContent.innerText = 'L\'utilisateur a refusé la géolocalisation.';
-                        console.log("L'utilisateur a refusé la géolocalisation.");
-                        break;
-                    case error.POSITION_UNAVAILABLE:
-                        // La position n'a pas pu être déterminée
-                        mainContent.innerText = 'La position n\'a pas pu être déterminée.';
-                        console.log("La position n'a pas pu être déterminée.");
-                        break;
-                    case error.TIMEOUT:
-                        // La demande de géolocalisation a expiré
-                        mainContent.innerText = 'La demande de géolocalisation a expiré.';
-                        console.log("La demande de géolocalisation a expiré.");
-                        break;
-                    case error.UNKNOWN_ERROR:
-                        // Une erreur inconnue s'est produite
-                        mainContent.innerText = 'Une erreur inconnue s\'est produite.';
-                        console.log("Une erreur inconnue s'est produite.");
-                        break;
-                }
-            }
-
-                function validPosition(position) {
+                navigator.geolocation.getCurrentPosition(function(position) {
                 // Récupère la localisation
                 var latitude = position.coords.latitude;
                 var longitude = position.coords.longitude;
@@ -194,8 +153,9 @@
                     
                     conn.send(JSON.stringify(disconnectionData));
                 };
-            }
+            })
         }
+    }
     </script>
 
     <!-- Fenetre modale users -->
