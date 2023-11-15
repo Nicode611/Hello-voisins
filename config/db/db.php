64d86e63ebcfbc1529b23c95930fa12caebed1c8
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-11-15 16:13:03 +0100

    16:12

--- config/db/db.php
+++ config/db/db.php
@@ -1,9 +1,14 @@
 <?php
     
-    $db_host = "mysql-garage-v-parrot.alwaysdata.net";
-    $db_user = "331032";
-    $db_pass = "Beta2k15";
-    $db_name = "hello-voisins_2023";
+    // $db_host = "mysql-garage-v-parrot.alwaysdata.net";
+    // $db_user = "331032";
+    // $db_pass = "Beta2k15";
+    // $db_name = "hello-voisins_2023";
     // $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    $db_host = getenv('DB_HOST');
+    $db_user = getenv('DB_USER');
+    $db_pass = getenv('DB_PASSWORD');
+    $db_name = getenv('DB_NAME');
 
 ?>
