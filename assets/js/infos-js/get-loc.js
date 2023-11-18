84a345e7e329212f89e115a5da77a2938978a4bc
Your Name <nicode611@gmail.com>
2023-11-18 17:52:35 +0100

    17:25

--- assets/js/infos-js/get-loc.js
+++ assets/js/infos-js/get-loc.js
@@ -2,38 +2,38 @@ var selfId = document.querySelector(".self-user-id").textContent;
 
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
-            // Récupère la localisation
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
+        // Récupère la localisation
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
 
-            sendLocation(latitude, longitude);
-    
-        }, function(error) {
-                var mainContent = document.querySelector('.main-content');
-                switch (error.code) {
-                    case error.PERMISSION_DENIED:
-                        // L'utilisateur a refusé la demande de géolocalisation
-                        mainContent.innerText = 'L\'utilisateur a refusé la géolocalisation.';
-                        console.log("L'utilisateur a refusé la géolocalisation.");
-                        break;
-                    case error.POSITION_UNAVAILABLE:
-                        // La position n'a pas pu être déterminée
-                        mainContent.innerText = 'La position n\'a pas pu être déterminée.';
-                        console.log("La position n'a pas pu être déterminée.");
-                        break;
-                    case error.TIMEOUT:
-                        // La demande de géolocalisation a expiré
-                        mainContent.innerText = 'La demande de géolocalisation a expiré.';
-                        console.log("La demande de géolocalisation a expiré.");
-                        break;
-                    case error.UNKNOWN_ERROR:
-                        // Une erreur inconnue s'est produite
-                        mainContent.innerText = 'Une erreur inconnue s\'est produite.';
-                        console.log("Une erreur inconnue s'est produite.");
-                        break;
-                }
-            })
+        sendLocation(latitude, longitude);
+
+    }, function(error) {
+        var mainContent = document.querySelector('.main-content');
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                // L'utilisateur a refusé la demande de géolocalisation
+                mainContent.innerText = 'L\'utilisateur a refusé la géolocalisation.';
+                console.log("L'utilisateur a refusé la géolocalisation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                // La position n'a pas pu être déterminée
+                mainContent.innerText = 'La position n\'a pas pu être déterminée.';
+                console.log("La position n'a pas pu être déterminée.");
+                break;
+            case error.TIMEOUT:
+                // La demande de géolocalisation a expiré
+                mainContent.innerText = 'La demande de géolocalisation a expiré.';
+                console.log("La demande de géolocalisation a expiré.");
+                break;
+            case error.UNKNOWN_ERROR:
+                // Une erreur inconnue s'est produite
+                mainContent.innerText = 'Une erreur inconnue s\'est produite.';
+                console.log("Une erreur inconnue s'est produite.");
+                break;
         }
+    })
+}
         
     // Envoie la localisation a la BDD
     function sendLocation(latitude, longitude) {
