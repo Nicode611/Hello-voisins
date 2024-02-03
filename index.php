fa0a2d855e1fe4d52c2763d3c67cd33154324c96
Your Name <nicode611@gmail.com>
2024-02-03 01:34:10 +0100

    01:34

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="assets/css/global.css">
     <link rel="stylesheet" href="assets/css/index.css">
     <title>Hello voisins</title>
